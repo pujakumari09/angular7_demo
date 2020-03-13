59c896b15a001c7285e16e8c947c8eef06de0c20
PujaKumariMEAN <pujak1122@gmail.com>
2020-03-13 14:28:19 +0530

    another commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : "home", component : HomeComponent},
+  {path : "contactus", component : ContactusComponent},
+  {path : "new-cmp", component : NewCmpComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { } export const
+RoutingComponent = [HomeComponent, ContactusComponent, NewCmpComponent]; 
